3a07632ae594d33417b914000b45591d891c6ca5
Jiovan <jiiovan@unochapeco.edu.br>
2020-06-11 04:36:55 -0300

    Tela de contato com o formulario

--- MariaPenhaAPP/src/pages/contatos/style.js
+++ MariaPenhaAPP/src/pages/contatos/style.js
@@ -55,5 +55,33 @@ export default StyleSheet.create({
         marginLeft: 3,
         marginRight: 15,
         fontSize: 12
-    }
+    },
+    containerForm: {
+        marginHorizontal: 15,
+        marginBottom: 15
+    },
+    inputText:{ 
+        minHeight: 50, 
+        borderColor: '#707070', 
+        borderWidth: 1, 
+        paddingHorizontal: 15,
+        marginBottom: 15,
+        borderRadius: 4 
+    },
+    botaoSubmit: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+        borderRadius: 38,
+        backgroundColor: '#D44E5A',
+        width: '100%',
+        height: 60,
+        paddingHorizontal: 15
+        
+    },
+    textoBotaoSubmit: {
+        color: '#fff',
+        fontSize: 22,
+    },
 });
